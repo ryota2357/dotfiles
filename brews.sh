2af0c9696f406af6dd57f238ae73cbdc49496d08
2357 <61523777+ryota2357@users.noreply.github.com>
2022-04-14 20:12:29 +0900

    add visual-studio, docker

--- brews.sh
+++ brews.sh
@@ -6,6 +6,9 @@ brew install tmux
 brew install node
 brew install zplug
 
+# 最新バージョン以外の取得を可能にする
+brew tap homebrew/cask-versions
+
 # プラグイン周りの依存解消
 brew install pyenv
 brew install rbenv ruby-build
@@ -37,23 +40,26 @@ brew install bat
 # vim-prettier
 brew install prettier
 
+
 brew install --cask alfred
 brew install --cask android-studio
-brew install --cask visual-studio-code
-brew install --cask rider
-brew install --cask unity-hub
 brew install --cask blender
-brew install --cask dotnet-sdk
-brew install --cask github
-brew install --cask google-chrome
-brew install --cask firefox
-brew install --cask iterm2
-brew install --cask notion
 brew install --cask deepl
 brew install --cask discord
-brew install --cask slack
-brew install --cask numi
-brew install --cask toggl-track
-brew install --cask zoom
-brew install --cask microsoft-office
+brew install --cask docker
+brew install --cask dotnet-sdk
+brew install --cask firefox
+brew install --cask github
+brew install --cask google-chrome
 brew install --cask grammarly
+brew install --cask iterm2
+brew install --cask microsoft-office
+brew install --cask notion
+brew install --cask numi
+brew install --cask rider
+brew install --cask slack
+brew install --cask toggl-track
+brew install --cask unity-hub
+brew install --cask visual-studio
+brew install --cask visual-studio-code
+brew install --cask zoom
