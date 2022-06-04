592c3d6794fd5e3952c7308f300480fe4a3754c3
2357 <61523777+ryota2357@users.noreply.github.com>
2022-06-04 22:26:29 +0900

    fix clang-format

--- ln.sh
+++ ln.sh
@@ -7,12 +7,13 @@ ln_dots() {
 mkdir -p ~/.config/nvim/
 sheldon init --shell zsh
 
-ln_dots zsh/zshrc.zsh      ~/.zshrc
-ln_dots zsh/zprofile.zsh   ~/.zprofile
-ln_dots zsh/plugins.toml   ~/.sheldon/plugins.toml
-ln_dots tmux/tmux.conf     ~/.tmux.conf
-ln_dots vim/vimrc          ~/.config/nvim/init.vim
-ln_dots vim/vimrc          ~/.ideavimrc
-ln_dots tex/latexmkrc.perl ~/.latexmkrc
+ln_dots zsh/zshrc.zsh                ~/.zshrc
+ln_dots zsh/zprofile.zsh             ~/.zprofile
+ln_dots zsh/plugins.toml             ~/.sheldon/plugins.toml
+ln_dots tmux/tmux.conf               ~/.tmux.conf
+ln_dots vim/vimrc                    ~/.config/nvim/init.vim
+ln_dots vim/vimrc                    ~/.ideavimrc
+ln_dots vim/format/clang-format.yaml ~/.clang-format
+ln_dots tex/latexmkrc.perl           ~/.latexmkrc
 
 unset -f ln_dots
