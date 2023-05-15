f10cc24fb76bf6467628486c237d90ed25552ff3
2357 <61523777+ryota2357@users.noreply.github.com>
2023-05-15 10:35:08 +0900

    update

--- terminal/wezterm.lua
+++ terminal/wezterm.lua
@@ -1,24 +1,28 @@
 local wezterm = require('wezterm');
 local config = wezterm.config_builder()
 
+-- IME
+config.use_ime = true
+config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
+
+-- Appearance
 config.font = wezterm.font('PleckJP')
 config.font_size = 18.5
-
-config.use_ime = true
 config.enable_tab_bar = false
-
 config.colors = {
     cursor_bg = '#fffaf5',
     cursor_fg = 'black',
     cursor_border = '#fffaf5',
 }
 
+-- Key
 config.keys = {
+    { key = '¥',               action = wezterm.action.SendString('\\') },
+    { key = '¥', mods = 'ALT', action = wezterm.action.SendString('¥')  },
     { key = '+', mods = 'CMD', action = wezterm.action.IncreaseFontSize },
     { key = '-', mods = 'CMD', action = wezterm.action.DecreaseFontSize },
-    { key = '0', mods = 'CMD', action = wezterm.action.ResetFontSize },
+    { key = '0', mods = 'CMD', action = wezterm.action.ResetFontSize    },
 }
-
 config.mouse_bindings = {
   {
     event = { Up = { streak = 1, button = 'Left' } },
@@ -27,6 +31,12 @@ config.mouse_bindings = {
   },
 }
 
+-- Startup
+config.native_macos_fullscreen_mode = true
+wezterm.on('gui-startup', function(cmd)
+  local _, _, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():toggle_fullscreen()
+end)
 config.default_prog = {
     '/bin/zsh',
     '--login',
