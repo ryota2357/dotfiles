183559b7b18dd70b41a5bebdeccae5c73985ed29
2357 <61523777+ryota2357@users.noreply.github.com>
2023-08-29 23:04:39 +0900

    update

--- vim/denops/@ddu-sources/ui_select.ts
+++ vim/denops/@ddu-sources/ui_select.ts
@@ -4,8 +4,8 @@ import {
   BaseSource,
   type DduEvent,
   Item,
-} from "https://deno.land/x/ddu_vim@v3.5.1/types.ts";
-import type { Denops } from "https://deno.land/x/ddu_vim@v3.5.1/deps.ts";
+} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
+import type { Denops } from "https://deno.land/x/ddu_vim@v3.6.0/deps.ts";
 
 type SelectItem = {
   index: number;
