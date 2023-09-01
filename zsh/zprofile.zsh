516569d9b1fe10e88bfe7d93e4fd5d3ea6588c59
2357 <61523777+ryota2357@users.noreply.github.com>
2023-09-01 20:28:37 +0900

    update

--- zsh/zprofile.zsh
+++ zsh/zprofile.zsh
@@ -13,8 +13,9 @@ export PATH="$HOMEBREW_PREFIX/opt/llvm/bin:$PATH"
 export LDFLAGS="-L/$HOMEBREW_PREFIX/opt/llvm/lib"
 export CPPFLAGS="-I/$HOMEBREW_PREFIX/llvm/include"
 
-# nodebrew
-export PATH="$HOME/.nodebrew/current/bin:$PATH"
+# volta
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
 
 # dotnet tools
 export PATH="$HOME/.dotnet/tools:$PATH"
