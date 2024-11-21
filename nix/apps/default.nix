c65e43b181a358e1d3587b4a11d785cfac877339
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2024-11-21 19:30:21 +0900

    refactor to use genAttrs

--- nix/apps/default.nix
+++ nix/apps/default.nix
@@ -1,11 +1,13 @@
 { nixpkgs }:
 let
-  forAllSystems =
-    f:
-    builtins.foldl' (acc: system: acc // { ${system} = (f system); }) { } [
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
+  systems = [
+    "aarch64-darwin"
+    "aarch64-linux"
+    "x86_64-darwin"
+    "x86_64-linux"
+  ];
+  eachSystem = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+
   shellScriptHeader = ''
     set -euo pipefail
     ESC=$(printf '\033')
@@ -13,38 +15,36 @@ let
       printf "''${ESC}[32m==>''${ESC}[m ''${ESC}[1m%s''${ESC}[m\n" "$1"
     }
   '';
-in
-forAllSystems (
-  system:
-  let
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-    getExe = pkgs.lib.getExe;
-  in
-  {
-    home-manager-switch = {
-      type = "app";
-      program = toString (
-        pkgs.writeShellScript "home-manager" ''
-          ${shellScriptHeader}
-          message "home-manager switch --flake .#$1"
-          ${getExe pkgs.home-manager} switch --flake .#$1
-        ''
-      );
-    };
 
-    nix-darwin-switch = {
-      type = "app";
-      program = toString (
-        pkgs.writeShellScript "home-manager" ''
-          ${shellScriptHeader}
-          message "nix-darwin switch --flake .#$1"
-          nix run nix-darwin -- switch --flake .#$1
-          message "brew upgrade"
-          brew upgrade
-        ''
-      );
-    };
-  }
-)
+  getExe = nixpkgs.lib.getExe;
+in
+eachSystem (pkgs: {
+
+  home-manager-switch = {
+    type = "app";
+    program = toString (
+      pkgs.writeShellScript "home-manager-switch" ''
+        ${shellScriptHeader}
+        message "home-manager switch --flake .#$1"
+        ${getExe pkgs.home-manager} switch --flake .#$1
+      ''
+    );
+  };
+
+  nix-darwin-switch = {
+    type = "app";
+    program = toString (
+      pkgs.writeShellScript "nix-darwin-switch" ''
+        if [[ ! "${pkgs.system}" =~ ^(aarch64|x86_64)-darwin$ ]]; then
+          echo "System is not supported: ${pkgs.system}" >&2
+          exit 1
+        fi
+        ${shellScriptHeader}
+        message "nix-darwin switch --flake .#$1"
+        nix run nix-darwin -- switch --flake .#$1
+        message "brew upgrade"
+        brew upgrade
+      ''
+    );
+  };
+})
--- nix/formatter/default.nix
+++ nix/formatter/default.nix
@@ -1,16 +1,7 @@
 { nixpkgs }:
-builtins.foldl'
-  (
-    acc: system:
-    acc
-    // {
-      ${system} = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
-    }
-  )
-  { }
-  [
-    "aarch64-linux"
-    "aarch64-darwin"
-    "x86_64-darwin"
-    "x86_64-linux"
-  ]
+nixpkgs.lib.genAttrs [
+  "aarch64-linux"
+  "aarch64-darwin"
+  "x86_64-darwin"
+  "x86_64-linux"
+] (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style)
