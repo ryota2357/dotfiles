57dae8ecc4819415b8e7c94477c6b513dbaa7cb4
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2024-11-22 15:05:21 +0900

    update

--- config-files/_generated.nix
+++ config-files/_generated.nix
@@ -3,6 +3,7 @@
   "alacritty.toml" = ./. + "/alacritty.toml";
   "fd~ignore" = ./. + "/fd~ignore";
   "git~ignore" = ./. + "/git~ignore";
+  "karabiner_complex_modifications.json" = ./. + "/karabiner_complex_modifications.json";
   "latexmkrc" = ./. + "/latexmkrc";
   "wezterm.lua" = ./. + "/wezterm.lua";
 }
