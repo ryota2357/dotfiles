f1e6a967070a71e816b38200614c90714254d622
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2024-12-07 17:08:27 +0900

    update

--- nix/darwin/default.nix
+++ nix/darwin/default.nix
@@ -6,29 +6,12 @@
   default = nix-darwin.lib.darwinSystem {
     inherit system;
     modules = [
-      (
-        { pkgs, ... }:
-        {
-          nix = {
-            optimise.automatic = true;
-            gc = {
-              automatic = true;
-              interval = {
-                Hour = 9;
-                Minute = 0;
-              };
-              options = "--delete-older-than 10d";
-            };
-            settings = {
-              experimental-features = "nix-command flakes";
-              max-jobs = 8;
-            };
-          };
-          services.nix-daemon.enable = true;
-          system = import ./system.nix;
-          homebrew = import ./homebrew.nix;
-        }
-      )
+      {
+        services.nix-daemon.enable = true;
+        nix = import ./nix.nix;
+        system = import ./system.nix;
+        homebrew = import ./homebrew.nix;
+      }
     ];
   };
 }
