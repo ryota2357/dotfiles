24b43801c7745342a49d2c03b0ae2b97513820f9
ryota2357 <noreply@ryota2357.com>
2025-05-28 23:38:49 +0900

    update

--- vim/after/lsp/ts_ls.lua
+++ vim/after/lsp/ts_ls.lua
@@ -7,8 +7,8 @@ return {
         local fname = vim.api.nvim_buf_get_name(bufnr)
         local node_root = root_pattern("package.json")(fname)
         local other_root = root_pattern("deno.json", "astro.config.mjs", "astro.config.ts")(fname)
-        if node_root and other_root ~= nil then
-            on_dir(fname)
+        if node_root and other_root == nil then
+            on_dir(node_root)
             return
         end
     end,
