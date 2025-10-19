46df9d2eb5026abfc686701df29e340199e7ef90
ryota2357 <noreply@ryota2357.com>
2025-10-19 14:33:48 +0900

    add ryota2357.cachix.org to nix-darwin config

--- flake.nix
+++ flake.nix
@@ -26,7 +26,6 @@
       "https://ryota2357.cachix.org"
       "https://nix-community.cachix.org"
     ];
-
     extra-trusted-public-keys = [
       "ryota2357.cachix.org-1:1Biai//719bDFSnfV7FK0vKHw9C1b0S4/DQiNdkmWQo="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
